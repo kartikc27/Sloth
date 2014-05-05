f7af6ddf44b6ace787fc394e64c40431e37173cf
Kartik Chillakanti <chillaka@usc.edu>
2014-05-05 01:47:19 -0700

    Initial commit from local git

---
+++ Sloth2.0/KSCHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  KSCHomeViewController.h
+//  Sloth2.0
+//
+//  Created by Kartik Chillakanti on 5/5/14.
+//  Copyright (c) 2014 KSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KSCHomeViewController : UIViewController
+
+@end
