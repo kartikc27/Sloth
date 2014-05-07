414e9dca757d297a12a025c2d0d8d307a81a85c0
Kartik Chillakanti <chillaka@usc.edu>
2014-05-06 22:13:13 -0700

    finished basic frontend

---
+++ Sloth2.0/KSCLoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  KSCLoginViewController.h
+//  Sloth2.0
+//
+//  Created by Kartik Chillakanti on 5/6/14.
+//  Copyright (c) 2014 KSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
+
+
+@interface KSCLoginViewController : UIViewController
+
+@end
